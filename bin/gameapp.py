0af95800f7b3f32c76ddb0f51ba2739dc4f144fc
WangX <wangx@WangXs-MacBook-Air.local>
2017-12-01 02:04:35 +0800

    Add upload image in hero register, cancel link

--- bin/gameapp.py
+++ bin/gameapp.py
@@ -1,9 +1,10 @@
 import web
 from gothonweb import map
+from PIL import Image
 
 urls = (
-    '/game', 'GameEngine',
     '/', 'Index',
+    '/game', 'GameEngine',
 )
 
 app = web.application(urls, globals())
@@ -12,7 +13,8 @@ app = web.application(urls, globals())
 if web.config.get('_session') is None:
     store = web.session.DiskStore('sessions')
     session = web.session.Session(app, store,
-            initializer={'room': None, 'heroname': None})
+            initializer={'room': None, 
+                         'heroname': None})
     web.config._session = session
 else:
     session = web.config._session
@@ -26,21 +28,39 @@ class Index(object):
         return render.hero_register()
 
     def POST(self):
-        form = web.input(heroname=None)
+        form = web.input(heroname=None, headimage={})
         if form.heroname:
             session.heroname = form.heroname
         else:
             session.heroname = 'Anonym'
-        web.seeother("/game")
+
+        filedir = './static'
+        if 'headimage' in form:
+            filepath = form.headimage.filename.replace('\\','/')
+            filename = filepath.split('/')[-1]
+            fout = open(filedir + '/' + filename, 'wb')
+            fout.write(form.headimage.file.read())
+            fout.close()
+
+            infile = filedir + '/' + filename
+            outfile = infile + '.thumbnail'
+            im = Image.open(infile)
+            im.thumbnail((128, 128))
+            im.save(outfile, im.format)
+
+        return render.hero_register()
+#        web.seeother("/game")
 
 class GameEngine(object):
     def GET(self):
         if session.room:
-            return render.show_room(room=session.room, heroname=session.heroname)
+            return render.show_room(room=session.room,
+                                    heroname=session.heroname)
         # If directly use /game URL
         else:
             session.room = map.START
-            return render.show_room(room=session.room, heroname=session.heroname)
+            return render.show_room(room=session.room,
+                                    heroname=session.heroname)
 
     def POST(self):
         form = web.input(action=None)
